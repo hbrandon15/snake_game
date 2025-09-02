9b2dbd31a8fad6fa0c5ed51abf02b63a6f4b3c3e
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-09-02 15:33:51 -0500

    Implement high score persistence by reading from and writing to a file

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,7 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.penup()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.color("white")
         self.goto(0, 270)
         self.update_scoreboard()
@@ -25,9 +25,17 @@ class ScoreBoard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            self.high_score = self.update_high_score()
         self.score = 0
         self.update_scoreboard()
+
+    def read_high_score(self):
+        with open("data.txt") as data:
+            return int(data.read())
+
+    def update_high_score(self):
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.score}")
 
 
 
